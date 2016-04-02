9174eaf147c67033eeba878e87a61dcd1675be6d
scherbank <konstantin_scherban@epam.com>
2016-04-02 19:31:13 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,4 +6,7 @@
 #
 # Apache 2.0
 #
+#zabbix_srv_ip = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'zabbix-srv' }[1]['private_ip_address']
+#node.override['zabbix']['agent']['conf']['Server'] = node.override['zabbix']['agent']['conf']['ServerActive'] = zabbix_srv_ip
+
 include_recipe 'zabbix-agent::service'
